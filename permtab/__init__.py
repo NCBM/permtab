00469f6ff1e5ab8d8c52c0ca0c997a0f50b96c39
worldmozara <worldmozara@163.com>
2023-03-11 18:32:52 +0800

    Add core code

---
+++ permtab/__init__.py
@@ -0,0 +1,25 @@
+"""
+A simple permission rule parsing library.
+
+Format:
+    permtab uses a syntax similar to crontab, although it is actually a
+    reversed version.
+
+```
+    # *.permtab
+    <rulename> <filter ...>
+```
+
+    > NOTE: If any filter is determined successfully, the rule can be
+    > tendable.
+
+    Here is an example:
+
+```
+    myrule1     owner_1234  operator_8525   operator_8390
+    myrule2     text_hello  text_world      "text_hello world!"
+    *           user_any
+    # The tab rule with name '*' is considered a base rule.
+    # If not defined, the default rule will pass everything to other rules.
+```
+"""
---
+++ permtab/core.py
@@ -0,0 +1,57 @@
+from pathlib import Path
+import re
+import shlex
+from typing import Callable, Dict, Iterable, List, Tuple, Union
+
+
+REGISTERED_RULEFACTORY: List[
+    Tuple[re.Pattern[str], Callable[..., Callable[..., bool]]]
+] = []
+_REGISTERED_RULE: Dict[str, Callable[..., bool]] = {"*": lambda: True}
+REGISTERED_RULE = dict(_REGISTERED_RULE)
+
+
+def reset_factory() -> None:
+    global REGISTERED_RULEFACTORY
+    REGISTERED_RULEFACTORY = []
+
+
+def reset_rule() -> None:
+    global REGISTERED_RULE
+    REGISTERED_RULE = dict(_REGISTERED_RULE)
+
+
+def _defract(fp: Path) -> Iterable[List[str]]:
+    with fp.open(encoding="utf-8") as f:
+        for ln in f:
+            if p := shlex.split(ln, comments=True):
+                yield p
+
+
+def defract(
+    fp: Union[str, Path],
+    *,
+    defract_func: Callable[[Path], Iterable[List[str]]] = _defract
+) -> List[List[str]]:
+    fp = Path(fp)
+    return list(defract_func(fp))
+
+
+def digest_filter(sfi: str) -> Callable[..., bool]:
+    for pat, fac in REGISTERED_RULEFACTORY:
+        if (m := pat.match(sfi)) is not None:
+            return fac(*m.groups(), **m.groupdict())
+    return lambda *args, **kwargs: False
+
+
+def generate_rule(line: Iterable[str]) -> Tuple[str, Callable[..., bool]]:
+    name, *sfilter = line
+    ffilter: map[Callable[..., bool]] = map(digest_filter, sfilter)
+    def _verify(*args, **kwargs) -> bool:
+        return any(filt(*args, **kwargs) for filt in ffilter)
+    return name, _verify
+
+
+def update_rule(name: str, func: Callable[..., bool]) -> None:
+    global REGISTERED_RULE
+    REGISTERED_RULE.update({name: func})
